2b7b336caef1fb725d98982e5a621ddc7f5c0b82
Asdfif <sasha-burov@mail.ru>
2020-12-19 14:06:06 +0300

    изменен метод calculate
    в методе уменьшено ветвление

--- hand.rb
+++ hand.rb
@@ -20,10 +20,9 @@ class Hand
   def calculate
     hand_value = 0
     @cards.each do |card|
-      if card.card_nominal.instance_of?(Integer)
-        hand_value += card.card_nominal
-      else
         case card.card_nominal
+        when Integer
+          hand_value += card.card_nominal
         when 'A'
           if hand_value + 11 > 21
             hand_value += 1
@@ -33,7 +32,6 @@ class Hand
         else
           hand_value += 10
         end
-      end
     end
     hand_value
   end
